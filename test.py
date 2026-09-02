322be8b37cb03d3b6b07cda8f1b34c4f8832e74c
Finley <finley.a.connor@gmail.com>
2026-09-02 20:32:37 +0000

    Added classes

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
 newnumber = number1 + number2
 print("The sum of ",number1," and ",number2," is:", newnumber)
+
+for i in range(number1, number2 + 1):
+    print(i)
